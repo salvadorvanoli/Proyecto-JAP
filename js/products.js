086945e792db2c92e72bf9c2812d2dd01c69b000
carlosvieira <48003209+carlosalbertovieira@users.noreply.github.com>
2023-08-17 01:32:16 -0300

    updated and refactored products.js , add under-construction.html

--- js/products.js
+++ js/products.js
@@ -1,57 +1,72 @@
+// crea un de texto definido en options.element y le agrega atributos pasados por la variable options (class y textcontent) 
+function createText(options) {
+  const element = document.createElement(options.element);
+  element.classList.add(options.class);
+  element.textContent = options.text;
+
+  return element;
+}
+
+// crea un elemento IMG y le agrega atributos pasados por parámetro (class, src y alt) 
+function createImage(options) {
+  const imageElement = document.createElement("img");
+  imageElement.classList.add(options.class);
+  imageElement.src = options.image;
+  imageElement.alt = options.name;
+
+  return imageElement;
+}
+
+function displayProduct(product) {
+  // Crea el contenedor del producto
+  const productElement = document.createElement("div");
+  productElement.className = "product";
+
+  // crea elementos del contenedor (imagen, nombre, descripción, precio y cantidad de vendidos)
+  const contentList = [
+    createImage({ class: 'imageElement', image: product.image, name: product.name }),
+    createText({ element: 'h2', class: 'nameElement', text: product.name }),
+    createText({ element: 'p', class: 'descriptionElement', text: product.description }),
+    createText({ element: 'p', class: 'priceElement', text: `$${product.cost} ${product.currency}` }),
+    createText({ element: 'p', class: 'soldElement', text: `Vendidos: ${product.soldCount}` }),
+  ];
+
+
+  // agrega cada uno de los elementos al contenedor
+  contentList.forEach(item => productElement.appendChild(item));
+  // agrega el contenedor a la lista de elementos
+  const productList = document.getElementById("product-list");
+  productList.appendChild(productElement);
+}
+
 document.addEventListener("DOMContentLoaded", function () {
-    const productList = document.getElementById("product-list");
-  
-    // URL para obtener la lista de productos de la categoría 101 (Autos)
-    const categoryId = 101;
-    const productsUrl = `https://japceibal.github.io/emercado-api/cats_products/${categoryId}.json`;
-  
-    // Realizar la petición web
-    fetch(productsUrl)
-      .then(response => response.json())
-      .then(data => {
-        // Verifica que "products" sea un array dentro de la respuesta
-        const products = data.products;
-        if (Array.isArray(products)) {
-          // Construir la lista de productos en el DOM
-          products.forEach(product => {
-            const productElement = document.createElement("div");
-            productElement.className = "product";
-  
-            const imageElement = document.createElement("img");
-            imageElement.classList.add("imageElement");
-            imageElement.src = product.image;
-            imageElement.alt = product.name;
-  
-            const nameElement = document.createElement("h2");
-            nameElement.classList.add("nameElement");
-            nameElement.textContent = product.name;
-  
-            const descriptionElement = document.createElement("p");
-            descriptionElement.classList.add("descriptionElement");
-            descriptionElement.textContent = product.description;
-  
-            const priceElement = document.createElement("p");
-            priceElement.classList.add("priceElement");
-            priceElement.textContent = `$${product.cost} ${product.currency}`;
-  
-            const soldElement = document.createElement("p");
-            soldElement.classList.add("soldElement");
-            soldElement.textContent = `Vendidos: ${product.soldCount}`;
-  
-            productElement.appendChild(imageElement);
-            productElement.appendChild(nameElement);
-            productElement.appendChild(descriptionElement);
-            productElement.appendChild(priceElement);
-            productElement.appendChild(soldElement);
-  
-            productList.appendChild(productElement);
-          });
-        } else {
-          console.error("La respuesta de la API no contiene un array de productos.");
-        }
-      })
-      .catch(error => {
-        console.error("Error al obtener la lista de productos:", error);
-      });
-  });
+    
+  // consigue la categoria seleccionada en el index (seteada por index.js)
+  const categorySelected = localStorage.getItem("catID");
+  // si la categorica seleccionada no es 101 (Autos) se redirecciona a under-construction.html
+  if(categorySelected !== "101") {
+    window.location.href = "/under-construction.html";
+    return;
+  }
+
+  // URL para obtener la lista de productos de la categoría 101 (Autos)
+  const productsUrl = `https://japceibal.github.io/emercado-api/cats_products/${categorySelected}.json`;
+
+  // Realizar la petición web
+  fetch(productsUrl)
+    .then(response => response.json())
+    .then(data => {
+      // Verifica que "products" sea un array dentro de la respuesta
+      const products = data.products;
+      if (Array.isArray(products)) {
+        // Construir la lista de productos en el DOM
+        products.forEach(product => displayProduct(product));
+      } else {
+        console.error("La respuesta de la API no contiene un array de productos.");
+      }
+    })
+    .catch(error => {
+      console.error("Error al obtener la lista de productos:", error);
+    });
+});
   
