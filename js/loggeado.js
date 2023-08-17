f9752f3454caa17868b4dfb70ffa1a917c582ca3
salvadorvanoli <141060855+salvadorvanoli@users.noreply.github.com>
2023-08-17 14:23:49 -0300

    Corrección del redireccionamiento

--- js/loggeado.js
+++ js/loggeado.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", function(){
 
     if(valorVariable === "false" || (valorVariable != "true" && valorVariable != "false")){
         setTimeout(function() {
-            // window.location.href = "../login.html";
             window.location.replace('login.html');
         }, 3000);
     }
