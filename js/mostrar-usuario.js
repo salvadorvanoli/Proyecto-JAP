f68be40a56c34f665e763cc0f85cbe6893d627a4
salvadorvanoli <141060855+salvadorvanoli@users.noreply.github.com>
2023-08-17 18:54:38 -0300

    Agregué un botón para cerrar sesión

--- js/mostrar-usuario.js
+++ js/mostrar-usuario.js
@@ -1,11 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(){
 
+    // Lógica para mostrar el nombre de perfil en los html
+
     usuarioStorage = localStorage.getItem('username');
     let nombreDeUsuario = document.getElementById("nombreDeUsuario");
+    let cerrarSesion = document.getElementById("cerrarSesion");
 
-    if(usuarioStorage === null){
+    if(usuarioStorage === 'null'){
         nombreDeUsuario.textContent += "No se ha iniciado sesión - Redirigiendo";
+        cerrarSesion.style.display = "none";
+
     } else {
+        cerrarSesion.style.display = "block";
         if(usuarioStorage.length > 10){
             for(let i=0; i<10; i++){
                 nombreDeUsuario.textContent += usuarioStorage[i];
@@ -16,4 +22,11 @@ document.addEventListener("DOMContentLoaded", function(){
         }
     }
 
+    // Lógica para el botón de cerrar sesión
+
+    cerrarSesion.addEventListener("click", function(){
+        localStorage.setItem("loggeado", false);
+        localStorage.setItem("username", null);
+        window.location.replace('login.html');
+    });
 });
