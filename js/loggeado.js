ee42d3931b8143b3da4ec050989aaf87b3fcd549
salvadorvanoli <141060855+salvadorvanoli@users.noreply.github.com>
2023-08-18 18:00:01 -0300

    Agrego un let a la variable del js loggeado

--- js/loggeado.js
+++ js/loggeado.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-    valorVariable = localStorage.getItem('loggeado');
+    let valorVariable = localStorage.getItem('loggeado');
 
     if(valorVariable === "false" || (valorVariable != "true" && valorVariable != "false")){
         setTimeout(function() {
