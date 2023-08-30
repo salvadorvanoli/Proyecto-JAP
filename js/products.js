7da5f4a8452ae5d999f04447279c5bbc00fea235
Yared-Daniel <141077128+Yared-Daniel@users.noreply.github.com>
2023-08-29 23:02:05 -0300

    Filtro por precios

--- js/products.js
+++ js/products.js
@@ -39,6 +39,11 @@ function displayProduct(product) {
   productList.appendChild(productElement);
 }
 
+function clearProductList (){
+  const productList = document.getElementById("product-list");
+  productList.innerHTML = ""
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     
   // consigue la categoria seleccionada en el index (seteada por index.js)
@@ -53,6 +58,7 @@ document.addEventListener("DOMContentLoaded", function () {
     .then(data => {
       // Verifica que "products" sea un array dentro de la respuesta
       const products = data.products;
+      console.log("products: ",products)
       if (Array.isArray(products)) {
         // Construir la lista de productos en el DOM
         products.forEach(product => displayProduct(product));
@@ -63,5 +69,98 @@ document.addEventListener("DOMContentLoaded", function () {
     .catch(error => {
       console.error("Error al obtener la lista de productos:", error);
     });
-});
+  });
   
+  // Filtro por precios
+  const productList = document.getElementById("productList");
+  const filterButton = document.getElementById("filterButton");
+  const clearButton = document.getElementById("clearButton");
+  const minPriceInput = document.getElementById("minPrice");
+  const maxPriceInput = document.getElementById("maxPrice");
+  const sortPriceAscButton = document.getElementById("sortPriceAsc");
+  const sortPriceDescButton = document.getElementById("sortPriceDesc");
+  const sortRelevanceDescButton = document.getElementById("sortRelevanceDesc");
+
+  let productsData = []; //almacena los datos 
+  
+  // Al hacer click en el boton del filtro (filterButton)
+  filterButton.addEventListener("click", () => {
+    console.log("¡Botón clickeado! Mensaje en la consola.");
+    const minPrice = parseFloat(minPriceInput.value); //Obtenemos el valor del boton Precio Minimo
+    const maxPrice = parseFloat(maxPriceInput.value); //"  " del precio màximo
+    console.log(minPrice, maxPrice);
+
+
+    //Toma los datos del ARRAY
+    const categorySelected = localStorage.getItem("catID");
+    const productsUrl = `https://japceibal.github.io/emercado-api/cats_products/${categorySelected}.json`;
+    //funcion para pedir informacion
+    fetch(productsUrl)
+    .then(response => response.json())
+    .then(data => {
+      // Verifica que "products" sea un array dentro de la respuesta
+      let products = data.products;
+      console.log("nuevos products: ",products)
+      if (Array.isArray(products)) {
+        // Construir la lista de productos en el DOM
+       // products.forEach(product => displayProduct(product));
+        products = products.filter(product => {
+          console.log(product.cost, product.name)
+
+          if (product.cost >= minPrice && product.cost <= maxPrice) {
+            return true
+
+          } 
+          return false
+        })
+        console.log(products)
+
+        clearProductList()
+        products.forEach(product => displayProduct(product));
+        
+
+      } else {
+        console.error("La respuesta de la API no contiene un array de productos.");
+      }
+    })
+    .catch(error => {
+      console.error("Error al obtener la lista de productos:", error);
+    });
+    clearButton.addEventListener("click", () => {
+      minPriceInput.value = "";
+      maxPriceInput.value = "";
+      const categorySelected = localStorage.getItem("catID");
+
+
+      clearProductList ()
+  // URL para obtener la lista de productos de la categoría 101 (Autos)
+  const productsUrl = `https://japceibal.github.io/emercado-api/cats_products/${categorySelected}.json`;
+      
+  // Realizar la petición web
+  fetch(productsUrl)
+    .then(response => response.json())
+    .then(data => {
+      // Verifica que "products" sea un array dentro de la respuesta
+      const products = data.products;
+      console.log("products: ",products)
+      if (Array.isArray(products)) {
+        // Construir la lista de productos en el DOM
+        products.forEach(product => displayProduct(product));
+      } else {
+        console.error("La respuesta de la API no contiene un array de productos.");
+      }
+    })
+    .catch(error => {
+      console.error("Error al obtener la lista de productos:", error);
+    });
+
+    });
+    
+
+    
+});
+
+
+
+
+
