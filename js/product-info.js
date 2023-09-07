9ae34fd26e5fac99e2a1986bdccfb3db3e8e0cfa
salvadorvanoli <141060855+salvadorvanoli@users.noreply.github.com>
2023-09-07 10:19:00 -0300

    Solución parte 1 y 2

--- js/product-info.js
+++ js/product-info.js
@@ -25,16 +25,16 @@ function displayProduct(product) {
   // crea elementos del contenedor (imagen, nombre, descripción, precio y cantidad de vendidos)
   const contentList = [
     createText({ element: 'h2', class: 'name', text: product.name }),
-    createText({ element: 'p', class: 'description', text: product.description }),
-    createText({ element: 'p', class: 'cost', text: product.cost }),
-    createText({ element: 'p', class: 'category', text: product.category }),
+    createText({ element: 'p', class: 'description', text: `Descripción: ${product.description}` }),
+    createText({ element: 'p', class: 'cost', text: `Precio: $${product.cost}` }),
+    createText({ element: 'p', class: 'category', text: `Categoría: ${product.category}` }),
     createText({ element: 'p', class: 'soldCount', text: `Vendidos: ${product.soldCount}` }),
   ];
 
   // agrega cada uno de los elementos al contenedor
   contentList.forEach(item => productInfo.appendChild(item));
   // agrega el contenedor a la lista de elementos
-  const productInfoDiv = document.getElementById("container");
+  const productInfoDiv = document.getElementById("containerInfo");
   productInfoDiv.appendChild(productInfo);
 
   for(let i = 0; i<product.images.length; i++){
@@ -56,10 +56,9 @@ fetch(URL)
     // if (Array.isArray(info)) {
       // Construir la lista de productos en el DOM
     //   data.forEach(product => displayProduct(product));
-    console.log(data);
       displayProduct(data);
     // }
 })
 .catch(error => {
-    console.log(error)
+    console.log("Error: ", error)
 });
